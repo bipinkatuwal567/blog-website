e6b87c61059e03c7f8baa545c6f7b9a35c3b9326
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-06-20 16:31:31 +0545

    add animation page(framer motion)

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,67 +1,70 @@
 import { Link } from "react-router-dom";
 import InputComponent from "../components/input.component";
 import GoogleImg from "../imgs/google.png";
+import AnimationPage from "../common/page-animation";
 
 const UserAuthForm = ({ type }) => {
   return (
-    <section className="h-cover flex items-center justify-center">
-      <form className="w-[80%] max-w-[400px]">
-        <h2 className="font-gelasio capitalize text-3xl text-center mb-24">
-          {type === "sign-in" ? "Welcome back" : "Join us today"}
-        </h2>
+    <AnimationPage keyValue={type}>
+      <section className="h-cover flex items-center justify-center">
+        <form className="w-[80%] max-w-[400px]">
+          <h2 className="font-gelasio capitalize text-3xl text-center mb-24">
+            {type === "sign-in" ? "Welcome back" : "Join us today"}
+          </h2>
 
-        {type !== "sign-in" ? (
+          {type !== "sign-in" ? (
+            <InputComponent
+              name={"fullname"}
+              id={"fullname"}
+              placeHolder={"Full name"}
+              type={"text"}
+              icon="user"
+            />
+          ) : (
+            ""
+          )}
+
           <InputComponent
-            name={"fullname"}
-            id={"fullname"}
-            placeHolder={"Full name"}
-            type={"text"}
-            icon="user"
+            name={"email"}
+            type={"email"}
+            id={"email"}
+            placeHolder={"Email"}
+            icon="envelope"
           />
-        ) : (
-          ""
-        )}
 
-        <InputComponent
-          name={"email"}
-          type={"email"}
-          id={"email"}
-          placeHolder={"Email"}
-          icon="envelope"
-        />
+          <InputComponent
+            name={"password"}
+            type={"password"}
+            id={"password"}
+            placeHolder={"Password"}
+            icon="lock"
+          />
 
-        <InputComponent
-          name={"password"}
-          type={"password"}
-          id={"password"}
-          placeHolder={"Password"}
-          icon="lock"
-        />
+          <button type="submit" className="btn-dark center mt-14 py-2">
+            {type.replace("-", " ")}
+          </button>
 
-        <button type="submit" className="btn-dark center mt-14 py-2">
-          {type.replace("-", " ")}
-        </button>
+          <div className="w-[90%] flex items-center gap-4 relative my-5 opacity-10 text-black center">
+            <hr className="bg-black w-1/2" />
+            <p>Or</p>
+            <hr className="bg-black w-1/2" />
+          </div>
 
-        <div className="w-[90%] flex items-center gap-4 relative my-5 opacity-10 text-black center">
-          <hr className="bg-black w-1/2" />
-          <p>Or</p>
-          <hr className="bg-black w-1/2" />
-        </div>
+          <button className="btn-dark w-[90%] flex center justify-center items-center gap-4">
+            <img src={GoogleImg} alt="googlelogo" className="w-5" />
+            <p>Continue with Google</p>
+          </button>
 
-        <button className="btn-dark w-[90%] flex center justify-center items-center gap-4">
-          <img src={GoogleImg} alt="googlelogo" className="w-5" />
-          <p>Continue with Google</p>
-        </button>
-
-        <p className="text-dark-grey underline mt-5 text-center">
-          <Link to={type === "sign-in" ? "/sign-up" : "/sign-in"}>
-            {type === "sign-in"
-              ? "Don't have an account?"
-              : "Already a member?"}
-          </Link>
-        </p>
-      </form>
-    </section>
+          <p className="text-dark-grey underline mt-5 text-center">
+            <Link to={type === "sign-in" ? "/sign-up" : "/sign-in"}>
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already a member?"}
+            </Link>
+          </p>
+        </form>
+      </section>
+    </AnimationPage>
   );
 };
 
